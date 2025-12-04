cee0a0966826867132c8e00823e4a37aa1969ac5
Joe Van Valer <jmvanvaler@gmail.com>
2025-12-03 20:56:30 -0500

    critical fix of date filtering

--- data_cleaning.R
+++ data_cleaning.R
@@ -3,19 +3,18 @@ library(lubridate)
 
 # read in data
 data <- read.csv('game_gameinfo_gamesummary_linescore.csv')
-# initial checks
-head(data)
-names(data)
+
 
 # initial cleanup; filter for correct years
 data_filtered <- data %>%
   mutate(date = as.Date(game_date)) %>%
   filter(
-    year(date) >= 2014,
-    month(date) >= 7,
+    date <= as.Date("2022-07-01"),
+    date >= as.Date("2014-07-01"),
     season_type == "Regular Season"
   )
 
-View(data_filtered)
+View(head(data_filtered))
+View(tail(data_filtered))
 # 4299 games
 nrow(data_filtered)
